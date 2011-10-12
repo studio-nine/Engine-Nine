0a8603de3ce11a17343dd8fb27e1b4a2d603362a
yufeih <none@none>
2011-10-12 01:30:54 +0000

    Add the missing files
    git-svn-id: https://nine.svn.codeplex.com/svn@62922 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5
    
    --HG--
    extra : convert_revision : 5609e2d524d58b0d0e8683b13f7bf9fff7672bc7

---
+++ Framework/Nine.Content.Pipeline/Design/BlendStateConverter.cs
@@ -0,0 +1,71 @@
+﻿#region Copyright 2009 - 2011 (c) Engine Nine
+//=============================================================================
+//
+//  Copyright 2009 - 2011 (c) Engine Nine. All Rights Reserved.
+//
+//=============================================================================
+#endregion
+
+#region Using Directives
+using System;
+using System.Xaml;
+using System.ComponentModel;
+using System.Globalization;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Nine.Content.Pipeline.Graphics;
+#endregion
+
+namespace Nine.Content.Pipeline.Design
+{
+    class BlendStateConverter : TypeConverter
+    {
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return destinationType == typeof(string) || base.CanConvertTo(context, destinationType);
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string) && value is BlendState)
+            {
+                var blendState = (BlendState)value;
+                if (BlendStateSerializer.BlendStateEquals(blendState, BlendState.Additive))
+                    return "Additive";
+                if (BlendStateSerializer.BlendStateEquals(blendState, BlendState.AlphaBlend))
+                    return "AlphaBlend";
+                if (BlendStateSerializer.BlendStateEquals(blendState, BlendState.NonPremultiplied))
+                    return "NonPremultiplied";
+                if (BlendStateSerializer.BlendStateEquals(blendState, BlendState.Opaque))
+                    return "Opaque";
+                
+                throw new InvalidContentException("Unknown BlendState: " + value);
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                string blendState = value.ToString();
+                if (blendState == "Additive")
+                    return BlendState.Additive;
+                if (blendState == "AlphaBlend")
+                    return BlendState.AlphaBlend;
+                if (blendState == "NonPremultiplied")
+                    return BlendState.NonPremultiplied;
+                if (blendState == "Opaque")
+                    return BlendState.Opaque;
+
+                throw new InvalidContentException("Unknown BlendState: " + value);
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
---
+++ Framework/Nine.Content.Pipeline/Design/ContentReferenceConverter.cs
@@ -0,0 +1,56 @@
+﻿#region Copyright 2009 - 2011 (c) Engine Nine
+//=============================================================================
+//
+//  Copyright 2009 - 2011 (c) Engine Nine. All Rights Reserved.
+//
+//=============================================================================
+#endregion
+
+#region Using Directives
+using System;
+using System.Xaml;
+using System.ComponentModel;
+using System.Globalization;
+#endregion
+
+namespace Nine.Content.Pipeline.Design
+{
+    class ContentReferenceConverter : TypeConverter
+    {
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return destinationType == typeof(string) || base.CanConvertTo(context, destinationType);
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string) && value != null)
+            {
+                dynamic contentReference = value;
+                return contentReference.Filename;
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                // http://stackoverflow.com/questions/1723263/typeconverter-for-generic-type-used-in-xaml
+                var destinationTypeProvider = context.GetService<IDestinationTypeProvider>();
+                if (destinationTypeProvider == null)
+                {
+                    throw new NotSupportedException("IDestinationTypeProvider not found");
+                }
+                var destinationType = destinationTypeProvider.GetDestinationType();
+                return Activator.CreateInstance(destinationType, value.ToString());
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
