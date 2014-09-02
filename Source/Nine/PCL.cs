441a1a754b83fe1052e9cb891461110f929accb0
Eric Tuvesson <eric.tuvesson@gmail.com>
2014-09-02 23:37:54 +0200

    Added PCL Projects.

---
+++ Source/Nine/PCL.cs
@@ -0,0 +1,89 @@
+﻿
+namespace System.Xaml
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class AttachableMemberIdentifier
+    {
+        public Type DeclaringType 
+        {
+            get { return declaringType; } 
+        }
+        private Type declaringType;
+
+        public string MemberName 
+        {
+            get { return memberName; }
+        }
+        private string memberName;
+
+        public AttachableMemberIdentifier(Type declaringType, string memberName)
+        {
+            this.declaringType = declaringType;
+            this.memberName = memberName;
+        }
+
+        public static bool operator !=(AttachableMemberIdentifier left, AttachableMemberIdentifier right)
+        {
+            return left.DeclaringType != right.DeclaringType && left.MemberName != right.MemberName;
+        }
+
+        public static bool operator ==(AttachableMemberIdentifier left, AttachableMemberIdentifier right)
+        {
+            return left.DeclaringType == right.DeclaringType && left.MemberName == right.MemberName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+
+    public interface IAttachedPropertyStore
+    {
+        int PropertyCount { get; }
+
+        void CopyPropertiesTo(KeyValuePair<AttachableMemberIdentifier, object>[] array, int index);
+        bool RemoveProperty(AttachableMemberIdentifier attachableMemberIdentifier);
+        void SetProperty(AttachableMemberIdentifier attachableMemberIdentifier, object value);
+        bool TryGetProperty(AttachableMemberIdentifier attachableMemberIdentifier, out object value);
+    }
+}
+
+namespace System.Windows.Markup
+{
+    public sealed class ContentPropertyAttribute : Attribute
+    {
+        public string Name
+        {
+            get { return name; }
+        }
+        private string name;
+
+        public ContentPropertyAttribute()
+        {
+
+        }
+
+        public ContentPropertyAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    public abstract class MarkupExtension
+    {
+        protected MarkupExtension()
+        {
+
+        }
+
+        public abstract object ProvideValue(IServiceProvider serviceProvider);
+    }
+}
